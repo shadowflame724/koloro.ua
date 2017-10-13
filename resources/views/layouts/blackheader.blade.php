d126ea925385f52c1f31fc72c9a30fe98da365a8
glalex <oleksii.hlaholiev@gmail.com>
2017-10-13 17:33:25 +0300

    update

--- resources/views/layouts/blackheader.blade.php
+++ resources/views/layouts/blackheader.blade.php
@@ -1,7 +1,7 @@
 <header class="color_menu">
     <!-- begin container -->
     <div class="container">
-        <a href="/" class="logo_link"><img src="/files/images/logo2.png" class="logo" width="166" height="51" alt="koloro"></a>
+        <a href="/" class="logo_link"><img src="/files/images/koloro-logo.svg" class="logo" width="166" height="51" alt="koloro"></a>
 
         @include('layouts.headerlinks')
 
