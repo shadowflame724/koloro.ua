ea83e2786104980507c9e9badc11c14196b6b649
OstrikovG <ostgleal@gmail.com>
2017-10-17 16:59:38 +0300

    Try to fix ckeditor image downloading (1)

--- config/lfm.php
+++ config/lfm.php
@@ -41,8 +41,8 @@ return [
     // You should create routes to serve images if it is not set to public.
     'base_directory' => 'public',
 
-    'images_folder_name' => 'files/images/content',
-    'files_folder_name'  => 'files/images/content',
+    'images_folder_name' => '/files/images/content',
+    'files_folder_name'  => '/files/images/content',
 
 //    'shared_folder_name' => 'shares',
     'thumb_folder_name'  => 'thumbs',
