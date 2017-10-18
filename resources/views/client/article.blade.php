a20c94b8e8513799adcf1f26002086ce9624c438
glalex <oleksii.hlaholiev@gmail.com>
2017-10-18 16:08:38 +0300

    add meta-data for articles

--- resources/views/client/article.blade.php
+++ resources/views/client/article.blade.php
@@ -60,7 +60,7 @@
 
                     <div itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
                         <meta itemprop="url"
-                              content="https://test.koloro.com.ua/files/images/blog/{{ $article->image_id-> }}">
+                              content="https://test.koloro.com.ua/files/images/blog/{{ $article->image_id }}">
                         <meta itemprop="width" content="300">
                         <meta itemprop="height" content="300">
                     </div>
